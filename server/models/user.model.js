7e2efb90840dd353da06b422adbd9d6b8c2c4e54
mrinal1224 <iammrinalkc@gmail.com>
2026-09-02 11:59:20 +0530

    user model

---
+++ server/models/user.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+     },
+
+     username : {
+        type : String,
+        required : true,
+        unique : true
+     },
+     email : {
+        type : String,
+        required : true,
+        unique : true
+     },
+     password : {
+        type : String,
+        required : true
+     },
+
+     profileImage : {
+      type : String
+     },
+
+     followers : [],
+
+     followings : [],
+
+
+     posts : [],
+     stories : [],
+     reels : []
+
+
+} , {timestamps : true})
+
+
+const User = mongoose.model('User' , userSchema)
+
+export default User
