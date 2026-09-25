5a0db183f64f1ce7dc1228e88551025c59eae933
mrinal1224 <iammrinalkc@gmail.com>
2026-09-25 11:14:02 +0530

    post creation controller

--- server/controllers/post.controllers.js
+++ server/controllers/post.controllers.js
@@ -1,55 +1,55 @@
 import Post from "../models/post.model.js";
+import User from "../models/user.model.js";
 import uploadToCloudinary from "../utils/uploadCloudinary.js";
 
-export const createPost = async(req , res)=>{
-  try {
+export const createPost = async (req, res) => {
+    try {
 
-    // image , author , caption
+        // image , author , caption
 
-    const {caption} = req.body
+        const { caption } = req.body
 
         // write some validations
 
-    if(!caption.trim()){
-        return res.status(400).json({ message: 'Please add a Caption' })
+        if (!caption.trim()) {
+            return res.status(400).json({ message: 'Please add a Caption' })
+        }
+
+        if (caption.length > 500) {
+            return res.status(400).json({ message: 'Caption should be less than 500 characters' })
+        }
+
+        // upload the Image to cloudinary
+
+
+        let image;
+
+        if (req.file) {
+            const uploadedImage = await uploadToCloudinary(req.file.buffer)
+            image = uploadedImage.secure_url
+        }
+
+
+        const postCreated = await Post.create({
+            image,
+            caption,
+            author: req.user._id
+        })
+
+        // add the postID inside posts array of the User
+        User.findByIdAndUpdate(req.user._id, {
+            $push: { posts: postCreated.id }
+        })
+      // relate the post with userData(username , profileImage , name)
+        
+      const populatedPost = await Post.findById(postCreated.id).populate('author' , "name username profileImage")
+
+
+        // send the Response
+
+        res.status(201).json({ message: "Post Created", post: populatedPost })
+
+    } catch (error) {
+
     }
-
-    if(caption.length> 500){
-        return res.status(400).json({ message: 'Caption should be less than 500 characters' })
-    }
-
-   // upload the Image to cloudinary
-
-
-   let image; 
-
-   if(req.file){
-       const uploadedImage =  await uploadToCloudinary(req.file.buffer)
-       image = uploadedImage.secure_url
-   }
-
-
-    const postCreated =  await Post.create({
-        image,
-        caption,
-        author : req.user._id
-     })
-
-     // add the postID inside posts array of the User
-     
-
-
-    
-
-    // relate the post with userData(username , profileImage , name)
-
-
-
-    // send the Response
-
-    res.status(201).json({message : "Post Created" , post : postCreated})
-    
-  } catch (error) {
-    
-  }
 }
